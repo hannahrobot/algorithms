7bb4e1e19547079784c789e56e05b05d6fb38a79
hannah <contactuser2220@gmail.com>
2021-02-24 11:21:56 -0500

    update: uniquepaths

--- basics/asyncAlgosUniquePaths.js
+++ basics/asyncAlgosUniquePaths.js
@@ -50,20 +50,22 @@ approaches:
   clues:
     can only go down and right
     check every possible path from each cell down and right
-    save the cell's paths to a memo
+    save the cell's paths to a memo/hash
     work from last cell backwards
 
 -----------------------------------
 
   top down:
     basecase: out of bounds: return 0;
-    basecase: (0,0): return 1;
+    basecase: (1,1): return 1;
 
     recursive case:
     return func(m-1, n) + func(m, n-1)
 
-  time: 0(2^n)
+  time: 0(2^n*m)
   space: 0(m+n) worst case size of recursion callstack is m+n
+
+  **top down can be improved with a hash. if you draw out the recursion as a tree, notice that we visit many of the cells multiple times, instead of recalculating that cell's paths, lets save the number of paths from that cell to a hash table and reference it next time we land on the cell.
 
 ------------------------------------
   top down w/ memoization:
@@ -78,7 +80,7 @@ approaches:
     memo[m,n] = func(m-1, n) + func(m, n-1)
     return memo[m,n]
 
-  time: 0(2^n)
+  time: 0(n*m)
   space: 0(m+n) worst case size of recursion callstack is m+n
 
 -------------------------------------
@@ -124,55 +126,54 @@ bottom up setup view
  */
 
 //top down
-// function uniquePaths(m, n) {
-//   //basecase out of bounds: we dont count it
-//   if(m <= 0 || n <= 0){
-//     return 0
-//   }
-//   //basecase: made it to end - we count it as one path
-//   //first row and first column count as one, so we stop before 0,0
-//   if(m === 1 || n === 1) {
-//     return 1
-//   }
+function uniquePaths(m, n) {
+  //basecase out of bounds: we dont count it
+  if (m <= 0 || n <= 0) {
+    return 0;
+  }
+  //basecase: made it to end - we count it as one path
+  //first row and first column count as one, so we stop before 0,0
+  if (m === 1 || n === 1) {
+    return 1;
+  }
 
-//   //recursive case
-//   return uniquePaths(m-1, n) + uniquePaths(m, n-1)
-// }
+  //recursive case
+  return uniquePaths(m - 1, n) + uniquePaths(m, n - 1);
+}
 
 //top down w/ memoization
-// function uniquePaths(m, n) {
+function uniquePaths(m, n) {
+  const memo = [];
 
-// const memo = []
+  //we use a helper func so we can place our memo in the outer func and have global access
+  function recurse(row, column) {
+    //basecase out of bounds: we dont count it
+    if (row <= 0 || column <= 0) {
+      return 0;
+    }
+    //basecase: made it to end - we count it as one path
+    //first row and first column count as one, so we stop before 0,0
+    if (row === 1 && column === 1) {
+      return 1;
+    }
 
-// //we use a helper func so we can place our memo in the outer func and have global access
-//   function recurse(row, column){
-//     //basecase out of bounds: we dont count it
-//     if(row <= 0 || column <= 0){
-//       return 0
-//     }
-//     //basecase: made it to end - we count it as one path
-//     //first row and first column count as one, so we stop before 0,0
-//     if(row === 1 && column === 1) {
-//       return 1
-//     }
+    //check if we've visited this cell before
+    //if yes, we can just return its value
+    if (memo.hasOwnProperty(`${row}${column}`)) {
+      return memo[`${row}${column}`];
+    }
 
-//     //check if we've visited this cell before
-//     //if yes, we can just return its value
-//     if(memo.hasOwnProperty(`${row}${column}`)){
-//       return memo[`${row}${column}`]
-//     }
+    //recursive case
+    //we save the result to our memo
+    memo[`${row}${column}`] =
+      recurse(row - 1, column) + recurse(row, column - 1);
 
-//     //recursive case
-//     //we save the result to our memo
-//     memo[`${row}${column}`] = recurse(row-1, column) + recurse(row, column-1)
+    //return the memo value
+    return memo[`${row}${column}`];
+  }
 
-//     //return the memo value
-//     return memo[`${row}${column}`]
-//   }
-
-//   return recurse(m, n)
-
-// }
+  return recurse(m, n);
+}
 
 // console.log(uniquePaths(7, 3))
 
@@ -201,4 +202,4 @@ function uniquePaths(m, n) {
 // time: 0(m*n)
 // space: 0(m*n)
 
-console.log(uniquePaths(3, 4));
+console.log(uniquePaths(3, 3));
